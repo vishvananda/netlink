b7e745465543c3a486b9524f1169103b00750306
Hubert <hubert.krauze@ovh.net>
2015-04-02 14:03:27 +0200

    add better filtering routes with RouterFilter add more Route filed info make Route String() function print preatty routes

--- route.go
+++ route.go
@@ -3,6 +3,7 @@ package netlink
 import (
 	"fmt"
 	"net"
+	"syscall"
 )
 
 // Route represents a netlink route. A route is associated with a link,
@@ -10,17 +11,94 @@ import (
 // gateway. Advanced route parameters and non-main routing tables are
 // currently not supported.
 type Route struct {
-	LinkIndex int
-	Scope     int
-	Protocol  int
-	Table     int
-	Type      int
-	Dst       *net.IPNet
-	Src       net.IP
-	Gw        net.IP
+	Iif      int
+	Oif      int
+	Scope    int
+	Protocol int
+	Table    int
+	Type     int
+	Family   int
+	Tos      int
+	Flags    int
+	Priority int
+	From     *net.IPNet
+	Dst      *net.IPNet
+	Src      net.IP
+	Gateway  net.IP
 }
 
 func (r Route) String() string {
-	return fmt.Sprintf("{Ifindex: %d Proto: %d Scope: %d Table: %d Type: %d Dst: %s Src: %s Gw: %s}",
-		r.LinkIndex, r.Protocol, r.Scope, r.Table, r.Type, r.Dst, r.Src, r.Gw)
+	return fmt.Sprintf("ip route scope %s protocol %s table %s %s via %s",
+		scopeToString(r.Scope), protocolToString(r.Protocol), tableToString(r.Table),
+		dstAndSrcToString(r.Dst, r.Src), r.Gateway)
+}
+
+func dstAndSrcToString(dst *net.IPNet, src net.IP) string {
+	if dst == nil && src == nil {
+		return "default"
+	}
+
+	var s string
+	if src != nil {
+		s += fmt.Sprintf("src %s", src)
+	}
+
+	if dst != nil {
+		s += fmt.Sprintf("dst %s", dst)
+	}
+	return s
+}
+
+var scopesString = map[int]string{
+	syscall.RT_SCOPE_HOST:     "host",
+	syscall.RT_SCOPE_LINK:     "link",
+	syscall.RT_SCOPE_SITE:     "site",
+	syscall.RT_SCOPE_NOWHERE:  "nowhere",
+	syscall.RT_SCOPE_UNIVERSE: "universe",
+}
+
+func scopeToString(scope int) string {
+	if s, ok := scopesString[scope]; ok {
+		return s
+	}
+	return fmt.Sprintf("unknown(%d)", scope)
+}
+
+var tablesString = map[int]string{
+	syscall.RT_TABLE_MAIN:    "main",
+	syscall.RT_TABLE_LOCAL:   "local",
+	syscall.RT_TABLE_DEFAULT: "default",
+	syscall.RT_TABLE_COMPAT:  "compat",
+	syscall.RT_TABLE_UNSPEC:  "unspec",
+}
+
+func tableToString(table int) string {
+	if s, ok := tablesString[table]; ok {
+		return s
+	}
+	return fmt.Sprintf("unknown(%d)", table)
+}
+
+var protocolsString = map[int]string{
+	syscall.RTPROT_UNSPEC:   "unspec",
+	syscall.RTPROT_REDIRECT: "redirect",
+	syscall.RTPROT_KERNEL:   "kernel",
+	syscall.RTPROT_BOOT:     "boot",
+	syscall.RTPROT_STATIC:   "static",
+	syscall.RTPROT_GATED:    "gated",
+	syscall.RTPROT_RA:       "ra",
+	syscall.RTPROT_MRT:      "mrt",
+	syscall.RTPROT_ZEBRA:    "zebra",
+	syscall.RTPROT_BIRD:     "bird",
+	syscall.RTPROT_DNROUTED: "dnrouted",
+	syscall.RTPROT_XORP:     "xorp",
+	syscall.RTPROT_NTK:      "ntk",
+	syscall.RTPROT_DHCP:     "dhcp",
+}
+
+func protocolToString(protocol int) string {
+	if s, ok := protocolsString[protocol]; ok {
+		return s
+	}
+	return fmt.Sprintf("unknown(%d)", protocol)
 }
--- route_linux.go
+++ route_linux.go
@@ -10,6 +10,48 @@ import (
 
 // RtAttr is shared so it is in netlink_linux.go
 
+// RouteFilter represents filter that can be apply to RouteList function.
+type RouteFilter struct {
+	Table    int
+	Protocol int
+	Scope    int
+	Type     int
+	Tos      int
+	Iif      int // the device from which this packet is expected to arrive.
+	Oif      int // force the output device on which this packet will be routed.
+
+	Flagmask uint64
+
+	// TODO: Implement rest of filter option
+	// Mark         int
+	// Realm        int
+	// Src          *net.IPNet
+	// RootVia      *net.IPNet
+	// ToRootDst    *net.IPNet
+	// ToMatchDst   *net.IPNet
+	// FromRootSrc  *net.IPNet
+	// FromMatchSrc *net.IPNet
+
+	// TODO: Implement Flush filter when needed.
+	// Flushed      int
+	// Flushb       string
+	// Flushp       int
+	// Flushe       int
+}
+
+// Flag mask for router filters. RouterFilter.Flagmask must be set to on
+// for filter to work.
+const (
+	FILTER_PROTOCOL uint64 = 1 << (1 + iota)
+	FILTER_SCOPE
+	FILTER_TYPE
+	FILTER_TOS
+	FILTER_IIF
+	FILTER_OIF
+	// FILTER_MARK
+	// FILTER_REAL
+)
+
 // RouteAdd will add a route to the system.
 // Equivalent to: `ip route add $route`
 func RouteAdd(route *Route) error {
@@ -25,7 +67,7 @@ func RouteDel(route *Route) error {
 }
 
 func routeHandle(route *Route, req *nl.NetlinkRequest) error {
-	if (route.Dst == nil || route.Dst.IP == nil) && route.Src == nil && route.Gw == nil {
+	if (route.Dst == nil || route.Dst.IP == nil) && route.Src == nil && route.Gateway == nil {
 		return fmt.Errorf("one of Dst.IP, Src, or Gw must not be nil")
 	}
 
@@ -64,17 +106,17 @@ func routeHandle(route *Route, req *nl.NetlinkRequest) error {
 		rtAttrs = append(rtAttrs, nl.NewRtAttr(syscall.RTA_PREFSRC, srcData))
 	}
 
-	if route.Gw != nil {
-		gwFamily := nl.GetIPFamily(route.Gw)
+	if route.Gateway != nil {
+		gwFamily := nl.GetIPFamily(route.Gateway)
 		if family != -1 && family != gwFamily {
 			return fmt.Errorf("gateway, source, and destination ip are not the same IP family")
 		}
 		family = gwFamily
 		var gwData []byte
 		if gwFamily == FAMILY_V4 {
-			gwData = route.Gw.To4()
+			gwData = route.Gateway.To4()
 		} else {
-			gwData = route.Gw.To16()
+			gwData = route.Gateway.To16()
 		}
 		rtAttrs = append(rtAttrs, nl.NewRtAttr(syscall.RTA_GATEWAY, gwData))
 	}
@@ -90,7 +132,7 @@ func routeHandle(route *Route, req *nl.NetlinkRequest) error {
 		b      = make([]byte, 4)
 		native = nl.NativeEndian()
 	)
-	native.PutUint32(b, uint32(route.LinkIndex))
+	native.PutUint32(b, uint32(route.Oif))
 
 	req.AddData(nl.NewRtAttr(syscall.RTA_OIF, b))
 
@@ -100,8 +142,10 @@ func routeHandle(route *Route, req *nl.NetlinkRequest) error {
 
 // RouteList gets a list of routes in the system.
 // Equivalent to: `ip route show`.
-// The list can be filtered by link and ip family.
-func RouteList(link Link, family int) ([]Route, error) {
+// The list can be filtered ip family and route filter.
+// By default (like ip route show) only routes are dumped, that
+// are in the MAIN table. To get all routes filter.Table has to be 0.
+func RouteList(family int, filter *RouteFilter) ([]Route, error) {
 	req := nl.NewNetlinkRequest(syscall.RTM_GETROUTE, syscall.NLM_F_DUMP)
 	msg := nl.NewIfInfomsg(family)
 	req.AddData(msg)
@@ -109,13 +153,6 @@ func RouteList(link Link, family int) ([]Route, error) {
 	msgs, err := req.Execute(syscall.NETLINK_ROUTE, syscall.RTM_NEWROUTE)
 	if err != nil {
 		return nil, err
-	}
-
-	index := 0
-	if link != nil {
-		base := link.Attrs()
-		ensureIndex(base)
-		index = base.Index
 	}
 
 	native := nl.NativeEndian()
@@ -127,7 +164,7 @@ func RouteList(link Link, family int) ([]Route, error) {
 			continue
 		}
 
-		if msg.Table != syscall.RT_TABLE_MAIN {
+		if filter == nil && msg.Table != syscall.RT_TABLE_MAIN {
 			// Ignore non-main tables
 			continue
 		}
@@ -137,17 +174,45 @@ func RouteList(link Link, family int) ([]Route, error) {
 			return nil, err
 		}
 
+		if filter != nil {
+			if msg.Table != uint8(filter.Table) {
+				continue
+			}
+			f := filter.Flagmask
+			switch {
+			case f&FILTER_PROTOCOL != 0:
+				if msg.Protocol != uint8(filter.Protocol) {
+					continue
+				}
+			case f&FILTER_SCOPE != 0:
+				if msg.Scope != uint8(filter.Scope) {
+					continue
+				}
+			case f&FILTER_TYPE != 0:
+				if msg.Type != uint8(filter.Type) {
+					continue
+				}
+			case f&FILTER_TOS != 0:
+				if msg.Tos != uint8(filter.Tos) {
+					continue
+				}
+			}
+		}
+
 		route := Route{
 			Scope:    int(msg.Scope),
 			Table:    int(msg.Table),
 			Protocol: int(msg.Protocol),
 			Type:     int(msg.Type),
+			Tos:      int(msg.Tos),
+			Family:   int(msg.Family),
+			Flags:    int(msg.Flags),
 		}
 
 		for j := range attrs {
 			switch attrs[j].Attr.Type {
 			case syscall.RTA_GATEWAY:
-				route.Gw = net.IP(attrs[j].Value)
+				route.Gateway = net.IP(attrs[j].Value)
 			case syscall.RTA_PREFSRC:
 				route.Src = net.IP(attrs[j].Value)
 			case syscall.RTA_DST:
@@ -155,14 +220,34 @@ func RouteList(link Link, family int) ([]Route, error) {
 					IP:   attrs[j].Value,
 					Mask: net.CIDRMask(int(msg.Dst_len), 8*len(attrs[j].Value)),
 				}
+			case syscall.RTA_SRC:
+				route.From = &net.IPNet{
+					IP:   attrs[j].Value,
+					Mask: net.CIDRMask(int(msg.Dst_len), 8*len(attrs[j].Value)),
+				}
 			case syscall.RTA_OIF:
 				routeIndex := int(native.Uint32(attrs[j].Value[0:4]))
-				if link != nil && routeIndex != index {
+				if filter != nil && filter.Flagmask&FILTER_OIF != 0 && filter.Oif != routeIndex {
 					// Ignore routes from other interfaces
 					continue
 				}
-				route.LinkIndex = routeIndex
+				route.Oif = routeIndex
+			case syscall.RTA_IIF:
+				routeIndex := int(native.Uint32(attrs[j].Value[0:4]))
+				if filter != nil && filter.Flagmask&FILTER_IIF != 0 && filter.Iif != routeIndex {
+					continue
+				}
+				route.Iif = routeIndex
+			case syscall.RTA_PRIORITY:
+				route.Priority = int(native.Uint32(attrs[j].Value[0:4]))
+			case syscall.RTA_TABLE:
+				// no action, table is storead already in msg.Table
+			default:
+				// Not Implemented
+				// TODO: Implement RTA_CACHEINFO, RTA_FLOW, RTA_MARK, flags&RTM_F_CLONED,
+				// RTA_METRICS, RTA_MULTIPATH
 			}
+
 		}
 		res = append(res, route)
 	}
@@ -210,7 +295,7 @@ func RouteGet(destination net.IP) ([]Route, error) {
 		for j := range attrs {
 			switch attrs[j].Attr.Type {
 			case syscall.RTA_GATEWAY:
-				route.Gw = net.IP(attrs[j].Value)
+				route.Gateway = net.IP(attrs[j].Value)
 			case syscall.RTA_PREFSRC:
 				route.Src = net.IP(attrs[j].Value)
 			case syscall.RTA_DST:
@@ -220,7 +305,7 @@ func RouteGet(destination net.IP) ([]Route, error) {
 				}
 			case syscall.RTA_OIF:
 				routeIndex := int(native.Uint32(attrs[j].Value[0:4]))
-				route.LinkIndex = routeIndex
+				route.Oif = routeIndex
 			}
 		}
 		res = append(res, route)
