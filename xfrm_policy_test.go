a123807666a726ff61f004e5dddab5398ff64c49
Alessandro Boch <aboch@users.noreply.github.com>
2016-05-09 09:19:18 -0700

    Allow to program L4 fields in policy selector (#113)
    Signed-off-by: Alessandro Boch <aboch@docker.com>

--- xfrm_policy_test.go
+++ xfrm_policy_test.go
@@ -1,6 +1,7 @@
 package netlink
 
 import (
+	"bytes"
 	"net"
 	"testing"
 )
@@ -12,9 +13,12 @@ func TestXfrmPolicyAddUpdateDel(t *testing.T) {
 	src, _ := ParseIPNet("127.1.1.1/32")
 	dst, _ := ParseIPNet("127.1.1.2/32")
 	policy := XfrmPolicy{
-		Src: src,
-		Dst: dst,
-		Dir: XFRM_DIR_OUT,
+		Src:     src,
+		Dst:     dst,
+		Proto:   17,
+		DstPort: 1234,
+		SrcPort: 5678,
+		Dir:     XFRM_DIR_OUT,
 		Mark: &XfrmMark{
 			Value: 0xabff22,
 			Mask:  0xffffffff,
@@ -38,6 +42,16 @@ func TestXfrmPolicyAddUpdateDel(t *testing.T) {
 
 	if len(policies) != 1 {
 		t.Fatal("Policy not added properly")
+	}
+
+	// Verify Selector fields
+	if !compareIPNet(policies[0].Dst, policy.Dst) ||
+		!compareIPNet(policies[0].Src, policy.Src) ||
+		policies[0].Proto != policy.Proto ||
+		policies[0].DstPort != policy.DstPort ||
+		policies[0].SrcPort != policy.SrcPort {
+		t.Fatalf("Incorrect policy data retrieved. Expected %v. Got %v.",
+			policy, policies[0])
 	}
 
 	// Modify the policy
@@ -64,4 +78,14 @@ func TestXfrmPolicyAddUpdateDel(t *testing.T) {
 	if len(policies) != 0 {
 		t.Fatal("Policy not removed properly")
 	}
+}
+
+func compareIPNet(a, b *net.IPNet) bool {
+	if a == b {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	return a.IP.Equal(b.IP) && bytes.Equal(a.Mask, b.Mask)
 }
