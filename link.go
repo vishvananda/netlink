0b8b6545ad48d2f8cb6127fc7a8fe767c92fd1f6
Alessandro Boch <aboch@docker.com>
2017-01-13 12:51:00 -0800

    Provide access to the 64 bits link statistics
    Signed-off-by: Alessandro Boch <aboch@docker.com>

--- link.go
+++ link.go
@@ -32,6 +32,7 @@ type LinkAttrs struct {
 	Namespace    interface{} // nil | NsPid | NsFd
 	Alias        string
 	Statistics   *LinkStatistics
+	Statistics64 *LinkStatistics64
 	Promisc      int
 	Xdp          *LinkXdp
 	EncapType    string
@@ -108,6 +109,34 @@ type LinkStatistics struct {
 	TxCompressed      uint32
 }
 
+/*
+Ref: struct rtnl_link_stats64 {...}
+*/
+type LinkStatistics64 struct {
+	RxPackets         uint64
+	TxPackets         uint64
+	RxBytes           uint64
+	TxBytes           uint64
+	RxErrors          uint64
+	TxErrors          uint64
+	RxDropped         uint64
+	TxDropped         uint64
+	Multicast         uint64
+	Collisions        uint64
+	RxLengthErrors    uint64
+	RxOverErrors      uint64
+	RxCrcErrors       uint64
+	RxFrameErrors     uint64
+	RxFifoErrors      uint64
+	RxMissedErrors    uint64
+	TxAbortedErrors   uint64
+	TxCarrierErrors   uint64
+	TxFifoErrors      uint64
+	TxHeartbeatErrors uint64
+	TxWindowErrors    uint64
+	RxCompressed      uint64
+	TxCompressed      uint64
+}
 type LinkXdp struct {
 	Fd       int
 	Attached bool
