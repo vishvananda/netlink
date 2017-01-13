b514ae35c6c68608a9cf408cc683b49ab05cc7ac
Xin He <hexin@jiedaibao.com>
2017-01-13 15:43:14 +0800

    fix a bug for net statistics when the whole network flow bytes is beyond 4GB, i.e. uint32
    Signed-off-by: Xin He <hexin@jiedaibao.com>
    
    	modified:   nl/nl_linux.go
    	modified:   nl/xfrm_state_linux_test.go

--- nl/nl_linux.go
+++ nl/nl_linux.go
@@ -363,7 +363,7 @@ func (req *NetlinkRequest) Execute(sockType int, resType uint16) ([][]byte, erro
 	if req.Sockets != nil {
 		if sh, ok := req.Sockets[sockType]; ok {
 			s = sh.Socket
-			req.Seq = atomic.AddUint32(&sh.Seq, 1)
+			req.Seq = atomic.AddUint64(&sh.Seq, 1)
 		}
 	}
 	sharedSocket := s != nil
@@ -438,7 +438,7 @@ func NewNetlinkRequest(proto, flags int) *NetlinkRequest {
 			Len:   uint32(syscall.SizeofNlMsghdr),
 			Type:  uint16(proto),
 			Flags: syscall.NLM_F_REQUEST | uint16(flags),
-			Seq:   atomic.AddUint32(&nextSeqNr, 1),
+			Seq:   atomic.AddUint64(&nextSeqNr, 1),
 		},
 	}
 }
@@ -688,7 +688,7 @@ func netlinkRouteAttrAndValue(b []byte) (*syscall.RtAttr, []byte, int, error) {
 // SocketHandle contains the netlink socket and the associated
 // sequence counter for a specific netlink family
 type SocketHandle struct {
-	Seq    uint32
+	Seq    uint64
 	Socket *NetlinkSocket
 }
 
