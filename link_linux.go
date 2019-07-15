b1e98597921720cee4147d6476110fc8fc56072d
Daniel Borkmann <daniel@iogearbox.net>
2019-07-15 19:54:20 +0200

    netlink: enforce similar pid checks as in iproute2
    iproute2's own netlink library asserts that the sockaddr sender pid
    has to be the one of the kernel [0]. It also doesn't bail out on pid
    mismatch but only skips the message instead. We've seen cases where
    the latter had a pid 0; in such case we should skip to the next nl
    message instead of hard bail out.
    
      [0] https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/lib/libnetlink.c
          rtnl_dump_filter_l(), __rtnl_talk_iov()
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- link_linux.go
+++ link_linux.go
@@ -1777,12 +1777,18 @@ func linkSubscribeAt(newNs, curNs netns.NsHandle, ch chan<- LinkUpdate, done <-c
 	go func() {
 		defer close(ch)
 		for {
-			msgs, err := s.Receive()
+			msgs, from, err := s.Receive()
 			if err != nil {
 				if cberr != nil {
 					cberr(err)
 				}
 				return
+			}
+			if from.Pid != nl.PidKernel {
+				if cberr != nil {
+					cberr(fmt.Errorf("Wrong sender portid %d, expected %d", from.Pid, nl.PidKernel))
+				}
+				continue
 			}
 			for _, m := range msgs {
 				if m.Header.Type == unix.NLMSG_DONE {
