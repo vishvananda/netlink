4b4422b43749e071a386f9d82a757498d385d71e
Sean M. Collins <sean@coreitpro.com>
2021-07-22 15:50:16 -0400

    Define more for conntact_unspecified
    This allows compilation on non-linux hosts for the containerlab project

--- conntrack_unspecified.go
+++ conntrack_unspecified.go
@@ -2,8 +2,14 @@
 
 package netlink
 
+import "net"
+
 // ConntrackTableType Conntrack table for the netlink operation
 type ConntrackTableType uint8
+
+const (
+	ConntrackTable = 1
+)
 
 // InetFamily Family type
 type InetFamily uint8
@@ -13,6 +19,32 @@ type ConntrackFlow struct{}
 
 // ConntrackFilter placeholder
 type ConntrackFilter struct{}
+
+type ConntrackFilterType uint8
+
+const (
+	ConntrackOrigSrcIP   = iota                // -orig-src ip    Source address from original direction
+	ConntrackOrigDstIP                         // -orig-dst ip    Destination address from original direction
+	ConntrackReplySrcIP                        // --reply-src ip  Reply Source IP
+	ConntrackReplyDstIP                        // --reply-dst ip  Reply Destination IP
+	ConntrackReplyAnyIP                        // Match source or destination reply IP
+	ConntrackOrigSrcPort                       // --orig-port-src port    Source port in original direction
+	ConntrackOrigDstPort                       // --orig-port-dst port    Destination port in original direction
+	ConntrackNatSrcIP    = ConntrackReplySrcIP // deprecated use instead ConntrackReplySrcIP
+	ConntrackNatDstIP    = ConntrackReplyDstIP // deprecated use instead ConntrackReplyDstIP
+	ConntrackNatAnyIP    = ConntrackReplyAnyIP // deprecated use instead ConntrackReplyAnyIP
+)
+
+func (f *ConntrackFilter) AddIP(tp ConntrackFilterType, ip net.IP) error {
+	return ErrNotImplemented
+}
+func (f *ConntrackFilter) AddPort(tp ConntrackFilterType, port uint16) error {
+
+	return ErrNotImplemented
+}
+func (f *ConntrackFilter) AddProtocol(proto uint8) error {
+	return ErrNotImplemented
+}
 
 // ConntrackTableList returns the flow list of a table of a specific family
 // conntrack -L [table] [options]          List conntrack or expectation table
