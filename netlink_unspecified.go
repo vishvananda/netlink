7db456fa39ec10753b725934eed89bfc47272a0a
Noah <noah@noah-wagner.com>
2022-04-20 17:11:43 -0700

    Add XfrmPolicyGet to unspecified operations

--- netlink_unspecified.go
+++ netlink_unspecified.go
@@ -216,6 +216,10 @@ func XfrmPolicyList(family int) ([]XfrmPolicy, error) {
 	return nil, ErrNotImplemented
 }
 
+func XfrmPolicyGet(policy *XfrmPolicy) (*XfrmPolicy, error) {
+	return nil, ErrNotImplemented
+}
+
 func XfrmStateAdd(policy *XfrmState) error {
 	return ErrNotImplemented
 }
