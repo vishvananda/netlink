6b0aa1db32a955c9972f09654edebf58b2c37c8c
Adrian Pistol <vifino@tty.sh>
2023-04-25 18:37:27 +0200

    link: add LinkSetIP6AddrGenMode

--- handle_unspecified.go
+++ handle_unspecified.go
@@ -171,6 +171,10 @@ func (h *Handle) LinkSetGROMaxSize(link Link, maxSize int) error {
 	return ErrNotImplemented
 }
 
+func (h *Handle) LinkSetIP6AddrGenMode(link Link, mode int) error {
+	return ErrNotImplemented
+}
+
 func (h *Handle) setProtinfoAttr(link Link, mode bool, attr int) error {
 	return ErrNotImplemented
 }
