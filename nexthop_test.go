6e61cd407d1d66dd67fc66aaaf5c2a42e0614426
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2025-10-28 23:29:05 -0700

    Add a basic support for nexthop
    Add a basic support of Linux's ip nexthop equivalent. In this PR, I
    specifically focused on implementing a minimal feature to accomplish
    IPv4 prefix with IPv6 (link-local) nexthop which is used by various
    implementation like FRR to support technique called BGP Unnumbered.
    
    The summary of the new features are:
    
    - Introduce a low level primitive for nexthop in the nl package
    - Introduce NexthopAdd/Del/List/Replace APIs (supports
      NHA_ID/BLACKHOLE/GATEWAY, and protocol field)
    - Introduce NHID field to the Route object which allows attaching
      nexthop to routes.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    === Squashed Commits ===
    
    nl: Fix some wrong error and done message handling
    
    The current logic of parsing ERROR and DONE message is, first reads
    error field and when NLM_F_ACK_TLVS exists, tries to read the original
    request header, payload of the request, and extended ACK.
    
    We have three issues here:
    
    1. The existence of the original request header is not indicated by
       NLM_F_ACK_TLVS flag. At least the original request header always
       exists.
    2. We are missing the check for NLM_F_CAPPED flag. When the flag exists,
       the payload of the request doesn't exist. In that case, we shouldn't
       try to skip the payload. Otherwise, we may end up with the
       out-of-range read.
    3. NLMSG_DONE doesn't contain the original request, so we shouldn't
       apply original request parsing logic to it.
    
    In this commit, we fix these issues by:
    
    1. We first check the existence of the NLM_F_CAPPED. When it exists,
       only skip the original request header. Otherwise, skip the payload as
       well. Don't apply this logic to the DONE message.
    2. After that, check the existence of the NLM_F_ACK_TLVS. When it
       exists, try to read extended ACK for both of DONE and ERROR messages.
       Otherwise, don't.
    
    Ref: https://docs.kernel.org/userspace-api/netlink/intro.html#netlink-message-types
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    nexthop: Add a low-level API for the nexthop
    
    Preparation for the support of the nexthop object.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    Add basic operation for nexthop
    
    Add a basic support of the Linux's nexthop object (ip nexthop XXX). This
    commit aims to introduce a basic operations (add, list, del) with
    minimal attributes. Further features can be added later incrementally.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    nexthop: Support NHA_OIF
    
    It can be used for expressing direct nexthop on specific link.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    nexthop: Support NHA_GATEWAY
    
    It can express an IP nexthop. A unique use case we can accomplish by
    this is attaching IPv6 nexthop to the routes with an IPv4 prefix which
    we cannot do with the existing `ip route` equivalents.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    nexthop: Support protocol
    
    Allow setting protocol for nexthop.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    route: Support RTA_NHID
    
    Support attaching nexthop object to route object via NHID field.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    
    nexthop: Add Replace operation support
    
    Add `ip nexthop replace` equivalent
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

---
+++ nexthop_test.go
@@ -0,0 +1,155 @@
+//go:build linux
+// +build linux
+
+package netlink
+
+import (
+	"net"
+	"slices"
+	"testing"
+
+	"golang.org/x/sys/unix"
+)
+
+func TestNexthopAddListDelReplace(t *testing.T) {
+	t.Cleanup(setUpNetlinkTest(t))
+
+	// get loopback interface
+	loop, err := LinkByName("lo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// bring the interface up
+	if err = LinkSetUp(loop); err != nil {
+		t.Fatal(err)
+	}
+
+	// create dummy interface
+	if err = LinkAdd(&Dummy{LinkAttrs: LinkAttrs{Name: "dummy0"}}); err != nil {
+		t.Fatal(err)
+	}
+
+	// get dummy interface
+	link0, err := LinkByName("dummy0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// bring the interface up
+	if err = LinkSetUp(link0); err != nil {
+		t.Fatal(err)
+	}
+
+	// Assign ip address to dummy interface
+	if err = AddrAdd(link0, &Addr{IPNet: &net.IPNet{
+		IP:   net.ParseIP("10.0.0.2"),
+		Mask: net.CIDRMask(24, 32),
+	}}); err != nil {
+		t.Fatal(err)
+	}
+
+	nh0 := &Nexthop{
+		// Manually assign ID
+		ID:        1,
+		Blackhole: true,
+	}
+
+	nh1 := &Nexthop{
+		// Auto assign ID
+		ID:       0,
+		OIF:      uint32(link0.Attrs().Index),
+		Gateway:  net.ParseIP("fe80::1234:5678:9abc:def0"),
+		Protocol: unix.RTPROT_BGP,
+	}
+
+	// Test NexthopAdd
+	if err = NexthopAdd(nh0); err != nil {
+		t.Fatal(err)
+	}
+
+	if err = NexthopAdd(nh1); err != nil {
+		t.Fatal(err)
+	}
+
+	// Test NexthopList
+	nhs, err := NexthopList()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(nhs) != 2 {
+		t.Fatalf("Expected 2 nexthop, got %d", len(nhs))
+	}
+
+	// Check that the ID assignment (both manual and automatic) worked
+	if nhs[0].ID == nhs[1].ID {
+		t.Fatalf("Duplicate nexthop IDs found: %d", nhs[0].ID)
+	}
+	idx := slices.IndexFunc(nhs, func(nh Nexthop) bool { return nh.ID == nh0.ID })
+	if idx == -1 {
+		t.Fatal("Manually assigned nexthop ID not found")
+	}
+	if nhs[1-idx].ID == 0 {
+		t.Fatal("Nexthop ID was not auto assigned")
+	}
+
+	resNH0 := nhs[idx]
+	resNH1 := nhs[1-idx]
+
+	// Check we can read what we wrote
+	if resNH0.Blackhole != nh0.Blackhole {
+		t.Fatalf("Nexthop Blackhole mismatch: expected %v, got %v", nh0.Blackhole, resNH0.Blackhole)
+	}
+	if resNH1.OIF != nh1.OIF {
+		t.Fatalf("Nexthop OIF mismatch: expected %d, got %d", nh1.OIF, resNH1.OIF)
+	}
+	if !resNH1.Gateway.Equal(nh1.Gateway) {
+		t.Fatalf("Nexthop Gateway mismatch: expected %s, got %s", nh1.Gateway, resNH1.Gateway)
+	}
+	if resNH1.Protocol != nh1.Protocol {
+		t.Fatalf("Nexthop Protocol mismatch: expected %s, got %s", nh1.Protocol, resNH1.Protocol)
+	}
+
+	// Test NexthopDel
+	if err = NexthopDel(nh0); err != nil {
+		t.Fatal(err)
+	}
+	nhs, err = NexthopList()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(nhs) != 1 {
+		t.Fatalf("Expected 1 nexthop, got %d", len(nhs))
+	}
+
+	// Test NexthopReplace
+	nh2 := &Nexthop{
+		// Replace nh1
+		ID:       resNH1.ID,
+		Protocol: unix.RTPROT_STATIC,
+		OIF:      uint32(link0.Attrs().Index),
+		Gateway:  net.ParseIP("10.0.0.1"),
+	}
+	if err = NexthopReplace(nh2); err != nil {
+		t.Fatal(err)
+	}
+	nhs, err = NexthopList()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(nhs) != 1 {
+		t.Fatalf("Expected 1 nexthop, got %d", len(nhs))
+	}
+
+	// Check we can read what we wrote
+	resNH2 := nhs[0]
+	if resNH2.Protocol != nh2.Protocol {
+		t.Fatalf("Nexthop Protocol mismatch: expected %s, got %s", nh2.Protocol, resNH2.Protocol)
+	}
+	if resNH2.OIF != nh2.OIF {
+		t.Fatalf("Nexthop OIF mismatch: expected %d, got %d", nh2.OIF, resNH2.OIF)
+	}
+	if !resNH2.Gateway.Equal(nh2.Gateway) {
+		t.Fatalf("Nexthop Gateway mismatch: expected %s, got %s", nh2.Gateway, resNH2.Gateway)
+	}
+}
