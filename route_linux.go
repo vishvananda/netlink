c56e8be51d603380f6c14c6942e7ee0e82d27d0f
lanphgphm <lanphuongpham02@gmail.com>
2026-04-16 16:40:17 +0700

    Enhance route type filter around RTA_DST address

--- route_linux.go
+++ route_linux.go
@@ -929,7 +929,11 @@ func (h *Handle) prepareRouteReq(route *Route, req *nl.NetlinkRequest, msg *nl.R
 		} else {
 			dstData = route.Dst.IP.To16()
 		}
-		rtAttrs = append(rtAttrs, nl.NewRtAttr(unix.RTA_DST, dstData))
+		if route.Type != unix.RTN_UNREACHABLE &&
+			route.Type != unix.RTN_BLACKHOLE &&
+			route.Type != unix.RTN_PROHIBIT {
+			rtAttrs = append(rtAttrs, nl.NewRtAttr(unix.RTA_DST, dstData))
+		}
 	} else if route.MPLSDst != nil {
 		family = nl.FAMILY_MPLS
 		msg.Dst_len = uint8(20)
